4302e18fb8f8ada0bb15e055c28bdeee8c730054
VC-Do3 <me.com>
2021-11-13 16:44:50 -0600

    Tercer commit, actualizacion de compatibilidad con csv y creacion de tabla

---
+++ main.js
@@ -0,0 +1,31 @@
+function Upload() {
+    var fileUpload = document.getElementById("fileupload");
+    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+    if (regex.test(fileUpload.value.toLowerCase())) {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var table = document.createElement("table");
+                var rows = e.target.result.split("\n");
+                for (var i = 0; i < rows.length; i++) {
+                    var cells = rows[i].split(",");
+                    if (cells.length > 1) {
+                        var row = table.insertRow(-1);
+                        for (var j = 0; j < cells.length; j++) {
+                            var cell = row.insertCell(-1);
+                            cell.innerHTML = cells[j];
+                        }
+                    }
+                }
+                var dvCSV = document.getElementById("dvCSV");
+                dvCSV.innerHTML = "";
+                dvCSV.appendChild(table);
+            }
+            reader.readAsText(fileUpload.files[0]);
+        } else {
+            alert("El navegador no soporta html 5");
+        }
+    } else {
+        alert("Formato de archivo no valido.");
+    }
+}
