d70c57e8976e650eac41837f6ec2f17e07a646ba
VC-Do3 <me.com>
2021-12-01 16:49:53 -0600

    No commit

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 //Esta funcion sirve para leer y
-//cargar los elementos de un archivo cs a una tabla html
+//cargar los elementos de un archivo csv a una tabla html
 
 function Upload() {
     var fileUpload = document.getElementById("fileupload");
     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
-    if (regex.test(fileUpload.value.toLowerCase())) {
-        if (typeof (FileReader) != "undefined") {
+
+    if (regex.test(fileUpload.value.toLowerCase())){
+    
+        if (typeof (FileReader) != "undefined"){
             var reader = new FileReader();
-            reader.onload = function (e) {
+            reader.onload = function (e){
                 var table = document.createElement("table");
                 var rows = e.target.result.split("\n");
-                for (var i = 0; i < rows.length; i++) {
+
+                for (var i = 0; i < rows.length; i++){
                     var cells = rows[i].split(",");
-                    if (cells.length > 1) {
+
+                    if (cells.length > 1){
                         var row = table.insertRow(-1);
-                        for (var j = 0; j < cells.length; j++) {
+
+                        for (var j = 0; j < cells.length; j++){
                             var cell = row.insertCell(-1);
                             cell.innerHTML = cells[j];
                         }
                     }
                 }
+
                 var dvCSV = document.getElementById("dvCSV");
                 dvCSV.innerHTML = "";
                 dvCSV.appendChild(table);
--- pos-html.php
+++ pos-html.php
@@ -12,11 +12,13 @@
 
 
     function joinFiles(array $files, $result) {
+        #requiere un array de archivos, y un tercer archivo para escribir los datos
+
         #lo que hace es leer una linea a la vez de un archivo hasta que acabe
         #y la escribe en el archivo resultado
         #hace esto con todos los files que esten en el array
         if(!is_array($files)) {
-            throw new Exception('`$files` deben de estar en un array');
+            throw new Exception('los archivos deben de estar en un array');
         }
 
         $wH = fopen($result, "w+");
@@ -24,32 +26,31 @@
         foreach($files as $file) {
             $fh = fopen($file, "r");
             while(!feof($fh)) {
-                fwrite($wH, fgets($fh));
+                fwrite($wH, fgets($fh)); #Lee una linea a la vez y la escribe
             }
             fclose($fh);
-            unset($fh);
+            unset($fh); #unset sirve para desmarcar la variable
             fwrite($wH, "\n");
         }
         fclose($wH);
         unset($wH);
     }
 
+    #AQUI COMIENZA TODO #####################################
 
-    
     $user = $_POST["user"]; #el user es el metodo de identificacion
     $user=strtolower($user);
 
     $task = $_POST["title"];
     $hour = $_POST["hour"];
     $desc = $_POST["desc"];
+
     if($user =="" || $task =="" || $hour==""){
         echo "Llena todos los campos";
     }
 
 
     $NW_DATA=array(array($task,$desc,$hour));
-
-
     NEW_CSV($NW_DATA,"temp");
 
 
@@ -66,15 +67,14 @@
 
         joinFiles(array("TEMPORAL.csv","temp.csv"),$user.".csv");
         unlink("temp.csv");
-        unlink("TEMPORAL.csv");
+        unlink("TEMPORAL.csv");#borra los archivo
         
         echo "Archivo guardado";
         header("Location: s-index.html"); #redirecciona al html
 
     }else{
         NEW_CSV($NW_DATA,$user);
-        unlink("temp.csv");
-        unlink("TEMPORAL.csv");
+        unlink("temp.csv"); #borra el archivo
 
         echo "Nuevo usuario agregado";
         header("Location: s-index.html"); #redirecciona al html
